211a7b99e83d910d9880c780086be6caef720988
Hubert Warchoł <n3tr00x99@gmail.com>
2026-05-22 19:08:27 +0200

    finalize create classes form

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -1,30 +1,28 @@
-import { CreateView } from '@/components/refine-ui/views/create-view.tsx';
 import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb.tsx';
+import { CreateView } from '@/components/refine-ui/views/create-view.tsx';
 import { Button } from '@/components/ui/button.tsx';
-import { useBack } from '@refinedev/core';
-import { Separator } from '@/components/ui/separator.tsx';
 import {
 	Card,
 	CardContent,
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card.tsx';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from '@refinedev/react-hook-form';
+import { Separator } from '@/components/ui/separator.tsx';
 import { classSchema } from '@/lib/schema.ts';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useBack, useList } from '@refinedev/core';
+import { useForm } from '@refinedev/react-hook-form';
 import * as z from 'zod';
 
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label.tsx';
 import {
 	Select,
 	SelectContent,
@@ -33,8 +31,9 @@ import {
 	SelectValue,
 } from '@/components/ui/select.tsx';
 import { Textarea } from '@/components/ui/textarea.tsx';
-import { Loader2 } from 'lucide-react';
 import UploadWidget from '@/components/upload-widget';
+import { Subject, User } from '@/types';
+import { Loader2 } from 'lucide-react';
 
 const Create = () => {
 	const back = useBack();
@@ -51,6 +50,7 @@ const Create = () => {
 	});
 
 	const {
+		refineCore: { onFinish },
 		handleSubmit,
 		formState: { isSubmitting, errors },
 		control,
@@ -58,35 +58,31 @@ const Create = () => {
 
 	const onSubmit = async (values: z.infer<typeof classSchema>) => {
 		try {
-			console.log(values);
+			await onFinish(values);
 		} catch (error) {
 			console.error('Error creating class:', error);
 		}
 	};
 
-	const teachers = [
-		{
-			id: 1,
-			name: 'John Doe',
+	const { query: subjectsQuery } = useList<Subject>({
+		resource: 'subjects',
+		pagination: {
+			pageSize: 100,
 		},
-		{
-			id: 2,
-			name: 'Jane Doe',
-		},
-	];
+	});
 
-	const subjects = [
-		{
-			id: 1,
-			name: 'Math',
-			code: 'MATH',
+	const { query: teachersQuery } = useList<User>({
+		resource: 'users',
+		filters: [{ field: 'role', operator: 'eq', value: 'teacher' }],
+		pagination: {
+			pageSize: 100,
 		},
-		{
-			id: 2,
-			name: 'English',
-			code: 'ENG',
-		},
-	];
+	});
+
+	const subjects = subjectsQuery.data?.data || [];
+	const subjectsLoading = subjectsQuery.isLoading;
+	const teachers = teachersQuery.data?.data || [];
+	const teachersLoading = teachersQuery.isLoading;
 
 	const bannerPublicId = form.watch('bannerCldPubId');
 
@@ -193,6 +189,7 @@ const Create = () => {
 												<Select
 													onValueChange={value => field.onChange(Number(value))}
 													value={field.value?.toString()}
+													disabled={subjectsLoading}
 												>
 													<FormControl>
 														<SelectTrigger className="w-full">
@@ -226,6 +223,7 @@ const Create = () => {
 												<Select
 													onValueChange={field.onChange}
 													value={field.value}
+													disabled={teachersLoading}
 												>
 													<FormControl>
 														<SelectTrigger className="w-full">
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,7 +1,7 @@
 import { createDataProvider, CreateDataProviderOptions } from '@refinedev/rest';
 
 import { BACKEND_BASE_URL } from '@/constants';
-import { ListResponse } from '@/types';
+import { CreateResponse, ListResponse } from '@/types';
 import { HttpError } from '@refinedev/core';
 
 if (!BACKEND_BASE_URL) {
@@ -67,6 +67,17 @@ const options: CreateDataProviderOptions = {
 
 			const payload: ListResponse = await response.clone().json();
 			return payload.pagination?.total || 0;
+		},
+	},
+	create: {
+		getEndpoint: ({ resource }) => resource,
+
+		buildBodyParams: async ({ variables }) => variables,
+
+		mapResponse: async response => {
+			const json: CreateResponse = await response.json();
+
+			return json.data ?? [];
 		},
 	},
 };
